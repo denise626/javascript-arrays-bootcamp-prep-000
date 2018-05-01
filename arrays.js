593d558eb841e5898ebe593efd92446c4a4fd8f4
April Kiser <denise626@hotmail.com>
2018-05-01 02:57:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(0, array.length - 2)
+  array = array.slice(0, array.length - 1)
   return array
 }
 
